979d1bd5b46d1793548a4c8f94b0f96b59f28238
mariellenolasco <mariellenolasco@gmail.com>
2019-10-04 11:13:33 -0500

    this is the initial ui and menu class

---
+++ Project0/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project0.App
+{
+    public class Menu
+    {
+        public void welcome()
+        {
+            
+            Console.WriteLine("Welcome! To the Bare Bones Bookstore");
+        top:
+            Console.WriteLine("Are you a new customer?");
+            Console.WriteLine("Input Y^(Yes) or N^(No)");
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "Y":
+                    Console.WriteLine("Please sign up before continuing");
+                    break;
+                case "N":
+                    Console.WriteLine("Nice to see you again! Please log in to continue");
+                    break;
+                default:
+                    Console.WriteLine("Input invalid");
+                    goto top;
+                    break;
+            }
+        }
+    }
+}
---
+++ Project0/Program.cs
@@ -0,0 +1,14 @@
+﻿using Project0.App;
+using System;
+
+namespace Project0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu start = new Menu();
+            start.welcome();
+        }
+    }
+}
